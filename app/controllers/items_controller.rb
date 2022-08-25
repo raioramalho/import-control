18d1090b449dd854e8652b74c9e2211014208ac3
Alan Ramalho <ramalho.sit@gmail.com>
2022-08-25 18:51:21 -0300

    css config and code fixed!

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -65,8 +65,7 @@ class ItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:registration_date, :arrival_date, :description, :price, :status, :validation, :programmed_to)
+      params.require(:item).permit(:code ,:registration_date, :arrival_date, :description, :price, :status, :validation, :programmed_to)
     end
-
 
 end
